59122ca51c222a20899bfda7d1f2f85a110fd1b5
GBAYI <ogungbayipaul61@gmail.com>
2020-03-27 22:05:50 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var name = 'paul';
+var courses = ['html','css','js'];
+//console.log(name,courses);
+var n = courses.length;
+//console.log(n);
+if(n % 2 === 0){
+for(var i = 0;i<200;i++){
+if(i % 2 === 0){
+console.log(courses[i]);
+}
+}
+}
+
